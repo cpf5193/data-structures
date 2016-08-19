4409db7379cefab7479bd5e7bcb2886f40cb1927
Christon Fukuhara <christon@yahoo-inc.com>
2016-08-18 21:14:56 -0700

    Add coin dynamic programming

--- data-structures/src/algorithms/Dynamic.java
+++ data-structures/src/algorithms/Dynamic.java
@@ -1,5 +1,8 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Dynamic {
 
   public static void main(String[] args) {
@@ -8,6 +11,11 @@ public class Dynamic {
     String expectedNDS = "[3, 4, 6, 7]";
     System.out.println("expected: " + expectedNDS);
     System.out.println("result: " + arrToString(resultNDS));
+    
+    int[] testCoins = {1, 5, 10, 25};
+    int[] actualCoins = minCoins(testCoins, 174);
+    System.out.println("expected: [1, 1, 1, 1, 10, 10, 25, 25, 25, 25, 25, 25]");
+    System.out.println("actual: " + arrToString(actualCoins));
   }
   
   public static String arrToString(int[] arr) {
@@ -45,6 +53,32 @@ public class Dynamic {
     for(int i=longestEndingAt[arr.length - 1] - 1; i >= 0; i--) {
       toReturn[i] = arr[index];
       index = prevIndex[index];
+    }
+    return toReturn;
+  }
+  
+  public static int[] minCoins(int[] coinVals, int n) {
+    ArrayList<Integer> selectedCoins = new ArrayList<Integer>();
+    int[] numCoins = new int[n + 1];
+    Arrays.fill(numCoins, Integer.MAX_VALUE);
+    numCoins[0] = 0;
+    int[] prevCoin = new int[n + 1];
+    for(int i=0; i<=n; i++) {
+      for (int coinVal : coinVals) {
+        if (coinVal <= i && numCoins[i - coinVal] + 1 < numCoins[i]) {
+          numCoins[i] = numCoins[i - coinVal] + 1;
+          prevCoin[i] = coinVal;
+        }
+      }
+    }
+    int index = n;
+    for (int i=numCoins[n]; i>0; i--) {
+      selectedCoins.add(prevCoin[index]);
+      index -= prevCoin[index];
+    }
+    int[] toReturn = new int[selectedCoins.size()];
+    for (int i=0; i<selectedCoins.size(); i++) {
+      toReturn[i] = selectedCoins.get(i);
     }
     return toReturn;
   }
